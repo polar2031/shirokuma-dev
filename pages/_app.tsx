0a93dc48688bcabfc487d8dfeaef8d722e3d35cf
KuanLun Chen <polar2031@gmail.com>
2022-02-01 14:30:39 +0800

    add basic mui theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,22 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#4b71a6',
+    },
+    secondary: {
+      main: '#bbdefb',
+    },
+  },
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (<ThemeProvider theme={theme}>
+    <Component {...pageProps} />
+  </ThemeProvider>)
 }
 
 export default MyApp
