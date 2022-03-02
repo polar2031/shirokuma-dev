335c7a07c0634d2a95628a769319a9e06da94279
Kuan-Lun Chen <polar2031@gmail.com>
2022-03-03 00:34:35 +0800

    feat: make list card style, add summary and tag

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -2,11 +2,17 @@ import { GetServerSideProps } from "next";
 import Link from "next/link";
 import {
   Box,
+  Card,
+  CardActions,
+  CardContent,
+  Chip,
   Container,
+  Grid,
   Link as MuiLink,
   Pagination,
   PaginationItem,
   Stack,
+  Typography,
 } from "@mui/material";
 import { getDefaultLayout } from "../component/layout";
 import {
@@ -27,20 +33,70 @@ const Blog = (props: {
     <>
       <ResponsiveAppBar title={props.title}></ResponsiveAppBar>
       <main>
-        <Container>
-          {props.articles.map((article) => {
-            return (
-              <Box m={1} key={article.canonicalUrl}>
-                <Link href={`/blog/${article.canonicalUrl}`} passHref>
-                  <MuiLink underline="none">{article.title}</MuiLink>
-                </Link>
-              </Box>
-            );
-          })}
+        <Container sx={{ marginY: 2 }}>
+          <Grid container spacing={2} sx={{ marginY: 2 }}>
+            {props.articles.map((article) => {
+              return (
+                <Grid item xs={12} md={6} key={article.canonicalUrl}>
+                  <Card sx={{ height: "250px" }}>
+                    <CardContent sx={{ height: "200px" }}>
+                      {/* title */}
+                      <Typography component="h2" variant="h5">
+                        {article.title}
+                      </Typography>
+
+                      {/* list tag */}
+                      <Stack
+                        direction="row"
+                        spacing={1}
+                        onMouseDown={(event) => event.stopPropagation()}
+                        onClick={(event) => {
+                          event.stopPropagation();
+                        }}
+                      >
+                        {article.tags.map((tag) => {
+                          return (
+                            <Link href={`/tags/${tag}`} key={tag} passHref>
+                              <Chip
+                                label={tag}
+                                size="small"
+                                key={tag}
+                                component={MuiLink}
+                              />
+                            </Link>
+                          );
+                        })}
+                      </Stack>
+
+                      {/* summary */}
+                      <Typography
+                        variant="subtitle1"
+                        paragraph
+                        sx={{
+                          display: "-webkit-box",
+                          WebkitBoxOrient: "vertical",
+                          WebkitLineClamp: 2,
+                          overflow: "hidden",
+                        }}
+                      >
+                        {article.summary}
+                      </Typography>
+                    </CardContent>
+                    <CardActions sx={{ justifyContent: "end" }}>
+                      <Link href={`/blog/${article.canonicalUrl}`} passHref>
+                        <MuiLink underline="none">Read more...</MuiLink>
+                      </Link>
+                    </CardActions>
+                  </Card>
+                </Grid>
+              );
+            })}
+          </Grid>
           <Box
             sx={{
               display: "flex",
               justifyContent: "center",
+              marginY: 2,
             }}
           >
             <Pagination
@@ -63,7 +119,6 @@ const Blog = (props: {
 };
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
-  // Run API calls in parallel
   let page: number;
   try {
     page = parseInt(context.query.page as string);
