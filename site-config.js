61d4b1010e88c19860169271a4562789b048db87
Kuan-Lun Chen <polar2031@gmail.com>
2022-04-05 15:33:47 +0800

    update config

--- site-config.js
+++ site-config.js
@@ -2,16 +2,35 @@
 const SiteConfig = {
   Variable: {
     // nav
-    title: "Your Awesome Site",
+    title: "Shirokuma.dev",
     // footer
-    copyright: "© 2022 YourName",
+    copyright: "© 2022 Shirokuma.dev",
     poweredBy: [{ name: "Next.js", url: "https://nextjs.org/" }],
-    siteUrl: "http://localhost:3000",
-    apiUrl: "http://localhost:1337",
+    // cms
+    apiUrl: "https://www.shirokuma.dev/strapi",
   },
   // check https://github.com/garmeeh/next-seo for more information
   // this part is equivalent to content in next-seo.config
-  SEO: {},
+  SEO: {
+    title: "Shirokuma.dev",
+    openGraph: {
+      type: "website",
+      title: "Shirokuma.dev",
+      url: "https://Shirokuma.dev",
+      site_name: "Shirokuma.dev",
+      images: [
+        {
+          url: "https://www.shirokuma.dev/strapi/uploads/temp_og_e4f7346e03.jpg",
+          alt: "Shirokuma.dev",
+        },
+      ],
+    },
+    twitter: {
+      handle: "@handle",
+      site: "@site",
+      cardType: "summary_large_image",
+    },
+  },
 };
 
 module.exports = SiteConfig;
